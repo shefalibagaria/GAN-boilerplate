7c073f21d45f798f044c9ecb3b22b0001b851f1b
Shefali Bagaria <sb518@ic.ac.uk>
2022-02-07 17:13:00 +0000

    add segmentation network

--- src/networks.py
+++ src/networks.py
@@ -20,38 +20,64 @@ def make_nets(config, training=True):
     else:
         config.load()
 
-    dk, ds, df, dp, gk, gs, gf, gp = config.get_net_params()
+    k, s, f, p = config.get_net_params()
 
     # Make nets
-    if config.net_type == 'gan':
-        class Generator(nn.Module):
-            def __init__(self):
-                super(Generator, self).__init__()
-                self.convs = nn.ModuleList()
-                self.bns = nn.ModuleList()
-                for lay, (k, s, p) in enumerate(zip(gk, gs, gp)):
-                    self.convs.append(nn.ConvTranspose2d(
-                        gf[lay], gf[lay+1], k, s, p, bias=False))
-                    self.bns.append(nn.BatchNorm2d(gf[lay+1]))
+    # if config.net_type == 'gan':
+    #     class Generator(nn.Module):
+    #         def __init__(self):
+    #             super(Generator, self).__init__()
+    #             self.convs = nn.ModuleList()
+    #             self.bns = nn.ModuleList()
+    #             for lay, (k, s, p) in enumerate(zip(gk, gs, gp)):
+    #                 self.convs.append(nn.ConvTranspose2d(
+    #                     gf[lay], gf[lay+1], k, s, p, bias=False))
+    #                 self.bns.append(nn.BatchNorm2d(gf[lay+1]))
 
-            def forward(self, x):
-                for conv, bn in zip(self.convs[:-1], self.bns[:-1]):
-                    x = F.relu_(bn(conv(x)))
-                out = torch.softmax(self.convs[-1](x), dim=1)
-                return out  # bs x n x imsize x imsize x imsize
+    #         def forward(self, x):
+    #             for conv, bn in zip(self.convs[:-1], self.bns[:-1]):
+    #                 x = F.relu_(bn(conv(x)))
+    #             out = torch.softmax(self.convs[-1](x), dim=1)
+    #             return out  # bs x n x imsize x imsize x imsize
 
-        class Discriminator(nn.Module):
+    #     class Discriminator(nn.Module):
+    #         def __init__(self):
+    #             super(Discriminator, self).__init__()
+    #             self.convs = nn.ModuleList()
+    #             for lay, (k, s, p) in enumerate(zip(dk, ds, dp)):
+    #                 self.convs.append(
+    #                     nn.Conv2d(df[lay], df[lay + 1], k, s, p, bias=False))
+
+            # def forward(self, x):
+            #     for conv in self.convs[:-1]:
+            #         x = F.relu_(conv(x))
+            #     x = self.convs[-1](x)  # bs x 1 x 1
+            #     return x
+
+    #     return Discriminator, Generator
+    
+    if config.net_type == 'cnn':
+        class Net(nn.Module):           # allows nn.Module functions to be used in the class
             def __init__(self):
-                super(Discriminator, self).__init__()
+                super().__init__()
                 self.convs = nn.ModuleList()
-                for lay, (k, s, p) in enumerate(zip(dk, ds, dp)):
+                for lay, (ker, str, pad) in enumerate(zip(k, s, p)):
                     self.convs.append(
-                        nn.Conv2d(df[lay], df[lay + 1], k, s, p, bias=False))
+                        nn.Conv2d(f[lay], f[lay+1], ker, str, pad)
+                    )
+                # self.conv1 = nn.Conv2d(1, 16, 3, padding=1)
+                # self.conv2 = nn.Conv2d(16, 16, 3, padding=1)
+                # self.conv3 = nn.Conv2d(16, 16, 3, padding=1)
+                # self.conv4 = nn.Conv2d(16, 3, 3, padding=1)
 
             def forward(self, x):
+                # x = F.relu(self.conv1(x))
+                # x = F.relu(self.conv2(x))
+                # x = F.relu(self.conv3(x))
+                # x = self.conv4(x)
                 for conv in self.convs[:-1]:
                     x = F.relu_(conv(x))
                 x = self.convs[-1](x)  # bs x 1 x 1
-                return x
+                return F.softmax(x, dim=1)
+    return Net
 
-        return Discriminator, Generator
